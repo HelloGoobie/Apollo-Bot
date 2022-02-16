5cc36e58f1f00e49264fe9cf2545a46639653a94
ActuallyGoobie <96952425+ActuallyGoobie@users.noreply.github.com>
2022-02-16 20:01:48 +0000

    updated function in functions.py
    changed display_avatar to avatar_url

--- Bot/modules/functions.py
+++ Bot/modules/functions.py
@@ -5,5 +5,5 @@ def dict_factory(cursor, row):
 
 def embed_generator(bot, description, colour = 0x00FF00):
     embed = discord.Embed(description=description, colour=colour)
-    embed.set_author(name=bot.user.name, icon_url= bot.user.display_avatar)
+    embed.set_author(name=bot.user.name, icon_url= bot.user.avatar_url)
     return embed
