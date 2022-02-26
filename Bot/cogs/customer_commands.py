d4723e3506ea56ff4c07e3d2e48c566bb589fbbc
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2022-02-26 22:49:26 +0000

    Update customer_commands.py

--- Bot/cogs/customer_commands.py
+++ Bot/cogs/customer_commands.py
@@ -64,7 +64,7 @@ class Customer(commands.Cog):
         final_cost = cost * amount
 
         if priority:
-            final_cost *= 1.05
+            final_cost = round(final_cost * 1.05)
 
         formatted_cost = "$" + format(final_cost, ",")
         name = (ctx.author.nick or ctx.author.name) + "#" + ctx.author.discriminator
