66462e13d55691e9dc16a90cc0b38e9156184dfe
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2022-03-01 22:13:58 +0000

    Add discount

--- Bot/modules/functions.py
+++ Bot/modules/functions.py
@@ -1,4 +1,6 @@
 import discord
+import sqlite3
+import time
 
 def dict_factory(cursor, row):
     return {col[0]: row[idx] for idx, col in enumerate(cursor.description)}
@@ -11,3 +13,45 @@ def embed_generator(bot, description, colour = 0x00FF00, author = None, avatar_u
     embed = discord.Embed(description=description, colour=colour)
     embed.set_author(name=author, icon_url=avatar_url)
     return embed
+
+def discount_active():
+    con = sqlite3.connect('db/orders.db')
+    con.row_factory = dict_factory
+    cur = con.cursor()
+
+    cur.execute("SELECT discount_id, discount_amount, discount_end_date FROM discount WHERE active = 1")
+    discount = cur.fetchone()
+    if not discount:
+        return False
+
+    if discount["discount_end_date"] < int(time.time()):
+        cur.execute("UPDATE discounts SET active = 0 WHERE active = 1")
+        con.commit()
+        con.close()
+        return False
+    con.close()
+    return [discount["discount_id"], discount["discount_amount"]]
+
+def discount_price(price):
+    con = sqlite3.connect('db/orders.db')
+    con.row_factory = dict_factory
+    cur = con.cursor()
+
+    cur.execute("SELECT discount_amount FROM discount WHERE active = 1")
+    discount = cur.fetchone()
+    con.close()
+    if not discount:
+        return price
+    return price * (1 - (discount["discount_amount"] / 100))
+
+def discount_get_amount(id):
+    con = sqlite3.connect('db/orders.db')
+    con.row_factory = dict_factory
+    cur = con.cursor()
+
+    cur.execute("SELECT discount_amount FROM discount WHERE discount_id = ?", (id,))
+    discount = cur.fetchone()
+    con.close()
+    if not discount:
+        return False
+    return discount["discount_amount"]
